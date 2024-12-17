15f80a7ba844a0349dd3034c9f3442c939d4355a
Nadiah <042574432@ecampus.ut.ac.id>
2024-12-17 17:19:06 +0700

    adjust error

--- src/app/page.js
+++ src/app/page.js
@@ -96,13 +96,13 @@ export default function Home() {
               doing it. So grab your hat, join the herd, and let&apos;s headbutt into
               the future together!&quot; */}
               // eslint-disable-next-line react/no-unescaped-entities
-              Welcome to the world of SHIROMO, where blockchain meets boldness and a touch of quirky charm! ✨ Born from the fast-paced Sonic lab, this white cat is a sacred animal in Japan, here for decentralized fun, community, and adventure. Whether you&apos;re here for the memes, the atmosphere, or just to worship this sacred cat, you&apos;re part of something truly unique. We&apos;re not just redefining speed on the blockchain; we&apos;re redefining how to have fun doing it. So, join the herd, and let&apos;s head into the future together!”
+              Welcome to the world of SHIROMO, where blockchain meets boldness and a touch of quirky charm! ✨ Born from the fast-paced Sonic lab, this white cat is a sacred animal in Japan, here for decentralized fun, community, and adventure. Whether you’re here for the memes, the atmosphere, or just to worship this sacred cat, you’re part of something truly unique. We’re not just redefining speed on the blockchain; we’re redefining how to have fun doing it. So, join the herd, and let’s head into the future together!”
 
-              What sets SHIROMO apart from the rest is not just its impeccable style - but also the technology underlying the entire ecosystem. Built on Sonic&apos;s cutting-edge blockchain infrastructure, the project combines the best of both worlds: the virality of meme culture and the toughness of serious technology.
+              What sets SHIROMO apart from the rest is not just its impeccable style - but also the technology underlying the entire ecosystem. Built on Sonic’s cutting-edge blockchain infrastructure, the project combines the best of both worlds: the virality of meme culture and the toughness of serious technology.
 
             </p>
             <p className="text-lg text-indigo-200 leading-relaxed">
-              What sets Goat with Hat apart isn&apos;t just its impeccable style – it&apos;s
+              What sets Goat with Hat apart isn’t just its impeccable style – it’s
               the underlying technology that powers the entire ecosystem. Built on
               Sonic cutting-edge blockchain infrastructure, this project combines
               the best of both worlds: the virality of meme culture and the
